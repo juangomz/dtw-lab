93857241de98def47cf33a7210848c617ebe461f
juangomz <juangoco02@gmail.com>
2025-02-04 18:29:15 +0100

    Error Fix

--- src/dtw_lab/lab1.py
+++ src/dtw_lab/lab1.py
@@ -5,8 +5,6 @@ import io
 import requests
 from typing import Literal, Union
 
-import pandas as pd
-
 def clean_data(df: pd.DataFrame) -> pd.DataFrame:
     """
     Clean and preprocess the input DataFrame.
